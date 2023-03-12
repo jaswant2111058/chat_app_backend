372a9ca99270a40ac6b5fbc9a1df8f75daf130ba
jaswant2111058 <jaswant2111058@akgec.ac.in>
2023-03-12 23:31:51 +0530

    ready for deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GCBURL||"http://localhost:5000/auth/google/callback",
+  callbackURL: process.env.GCBURL||"https://chat-app-swl5.onrender.com/auth/google/callback",
  scope:['profile','email'],
   passReqToCallback: true,
 },
