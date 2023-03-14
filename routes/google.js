81ced9fa29724cab59988b2eb459adbe6d846981
Jaswant Kushwaha <114646646+jaswant2111058@users.noreply.github.com>
2023-03-14 17:37:57 +0530

    v13_get_post

--- routes/google.js
+++ routes/google.js
@@ -37,7 +37,7 @@ router.get('/redirecting', (req, res) => {
   res.redirect('https://chat-app-tweeto.onrender.com')
 })
 
-router.get("/googlelogin",isLoggedIn,async (req, res) => {
+router.post("/googlelogin",isLoggedIn,async (req, res) => {
   const email = req.user.email;
   const semail = await schema.findOne({ email: email })
   if (!semail) {
